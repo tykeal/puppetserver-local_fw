4ffe55b4116996dadb09014bc7345bad4d7bd2c9
Andrew Grimberg <tykeal@bardicgrove.org>
2020-06-14 14:29:43 -0700

    Add ip6 post rules
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/post.pp
+++ manifests/post.pp
@@ -1,6 +1,7 @@
 # make sure rules in this class define before => undef
 
 class local_fw::post {
+  # ipv4
   firewall { '999 deny all other INPUT requests':
     chain  => 'INPUT',
     proto  => 'all',
@@ -15,5 +16,24 @@ class local_fw::post {
     action => 'reject',
     reject => 'icmp-host-prohibited',
     before => undef,
+  }
+
+  # ipv6
+  firewall { '999 deny all other INPUT requests v6':
+    chain    => 'INPUT',
+    proto    => 'all',
+    provider => 'ip6tables',
+    action   => 'reject',
+    reject   => 'icmp-host-prohibited',
+    before   => undef,
+  }
+  
+  firewall { '999 deny all other FORWARD requests v6':
+    chain    => 'FORWARD',
+    proto    => 'all',
+    provider => 'ip6tables',
+    action   => 'reject',
+    reject   => 'icmp-host-prohibited',
+    before   => undef,
   }
 }
